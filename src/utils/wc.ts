adc5009523b819ec61b8c5bc27422328c8cd0683
kashanshah <kashanshah@hotmail.com>
2022-08-01 00:30:27 +0400

    feat: updated keys

--- src/utils/wc.ts
+++ src/utils/wc.ts
@@ -3,8 +3,8 @@ import { IProduct } from './data';
 
 export const woocommerce = new WooCommerceRestApi({
   url: 'https://www.hurf.pk',
-  consumerKey: 'ck_9f39b08c6e1c6dde01e04c5c7bdd0c28e848a52e',
-  consumerSecret: 'cs_dc9fdbcf3f9e8be96f6850e4aaabab598f50afec',
+  consumerKey: 'ck_132ff8fe18d4b1bbc7ce8d128c70ed081d4231e4',
+  consumerSecret: 'cs_2132d497a1820fe4294b25d099c51b8613031c3b',
   version: 'wc/v3',
 });
 
